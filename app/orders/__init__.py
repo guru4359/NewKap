d60d0cba17c5a86ef3b607dc60599a3f656a8cd9
guru4359 <60113201+guru4359@users.noreply.github.com>
2025-07-26 12:17:16 -0500

    Update __init__.py

--- app/orders/__init__.py
+++ app/orders/__init__.py
@@ -3,3 +3,8 @@ from flask import Blueprint
 orders = Blueprint('orders', __name__)
 
 from app.orders import routes
+ flask import Blueprint
+
+orders = Blueprint('orders', __name__)
+
+from app.orders import routes
